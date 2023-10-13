9f9ace4ac38c54d127b174af67561418c8962344
Stephen J. Butler <stephen.butler@gmail.com>
2023-10-13 12:58:12 -0500

    Update some outputs

--- terraform/1.x-aws4/_outputs.tf
+++ terraform/1.x-aws4/_outputs.tf
@@ -1,3 +1,18 @@
+output "destination_credentials" {
+    value = {
+        arn  = aws_secretsmanager_secret.dest_credentials.arn
+        name = aws_secretsmanager_secret.dest_credentials.name
+    }
+}
+
+output "lambda_event" {
+    value = module.this_event
+}
+
+output "lambda_queue" {
+    value = module.this_queue
+}
+
 output "replicate_user" {
     value = {
         arn       = aws_iam_user.replicate.arn
